6a028942407b6c4dd15f1418f9864f049f15a98b
Abhishek Pratap Singh <1947shekabhi@gmail.com>
2023-07-28 01:13:10 +0530

    what is one way and two way data binding

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 
 const Home =() => {
 
+const [inputValue, setInputValue] = useState("");
+    console.log(inputValue)
+
+const onInputChange = (ev) => {
+setInputValue(ev.target.value);
+};
 return (
 <div>
-    Home
+    <div>{inputValue}</div>
+    <button type="button" onClick={() =>{
+        setInputValue('Abhishek')
+    }}>Update to random</button>
+    <input type="text" value ={inputValue} onChange={onInputChange}/>
 </div>
 
 );
