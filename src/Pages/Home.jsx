f1f35e3600c49989571830e7482b1b79c09151a4
Abhishek Pratap Singh <1947shekabhi@gmail.com>
2023-07-29 17:48:12 +0530

    isolated search form logic in SearchForm component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react';
 import {  searchForShows ,searchForPeople} from './../api/tvmaze';
+import SearchForm from '../Components/SearchForm';
 
 const Home = () => {
-  const [searchStr, setSearchStr] = useState('');
+  
   const [apiData, setApiData] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
-  const [ searchOption, setSearchOption] = useState('shows');
 
-  console.log(searchOption);
-
-  const onSearchInputChnage = ev => {
-    setSearchStr(ev.target.value);
-  };
-    
-    const onRadioChange = ev => {
-      setSearchOption(ev.target.value);
-    }
-
-  const onSearch = async ev => {
-    ev.preventDefault();
+  const onSearch = async ({q, searchOption}) => {
 
     try {
       setApiDataError(null);
 
     if(searchOption === 'shows'){
-      const result = await searchForShows(searchStr);
+      const result = await searchForShows(q);
       setApiData(result);
     }else{
-      const result = await searchForPeople(searchStr);
+      const result = await searchForPeople(q);
       setApiData(result);
     }
     } catch (error) {
@@ -49,20 +38,8 @@ const Home = () => {
 
   return (
     <div>
-      <form onSubmit={onSearch}>
-        <input type="text" value={searchStr} onChange={onSearchInputChnage} />
-
-        <label>
-          Shows
-          <input type="radio" name="search-option" value="shows" checked={searchOption === 'shows'} onChange={onRadioChange}/>
-        </label>
-        <label>
-          Actors
-          <input type="radio" name="search-option" value="actors" checked={searchOption === 'actors'} onChange={onRadioChange}/>
-        </label>
-
-        <button type="submit">Search</button>
-      </form>
+      <SearchForm onSearch={onSearch}/>
+      
       <div>{renderApiData()}</div>
     </div>
   );
