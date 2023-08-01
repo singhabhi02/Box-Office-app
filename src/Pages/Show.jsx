a4395e288b12258497d7a3d6ae1b23a9a80f1b8d
Abhishek Pratap Singh <1947shekabhi@gmail.com>
2023-08-01 11:06:56 +0530

    save the changes

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 const Show = () => {
     const {showId} = useParams();
     
-    return <div>Show page for show {showId}</div>;
+    return( <div>Show page for show {showId}</div>
+    );
 };
 export default Show;
